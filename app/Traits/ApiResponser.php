7b11bc344fc76e2461e3306a7419a62c366dbec1
jannah-hub <jannpals@gmail.com>
2021-05-29 15:54:23 +0800

    Changes as of May 29

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -12,7 +12,7 @@
 
     public function successResponse($data, $code = Response::HTTP_OK)
     {
-    return response()->json(['data' => $data], $code);
+    return response()->json(['data' => $data, 'site' => 1], $code);
     }
 
     /**
@@ -24,7 +24,7 @@
 
     public function errorResponse($message, $code)
     {
-    return response()->json(['error' => $message, 'code' => $code], 
+    return response()->json(['error' => $message, 'site' => 1,'code' => $code], 
     $code);
     }
 }
